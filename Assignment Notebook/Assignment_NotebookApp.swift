336e0ce8109643c4fa076dd2152f0fe331ea5fef
Edwin Tovar <poohwinnie2800@gmail.com>
2026-02-03 08:17:35 -0800

    Initial Commit

---
+++ Assignment Notebook/Assignment_NotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_NotebookApp.swift
+//  Assignment Notebook
+//
+//  Created by Edwin Tovar on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_NotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
