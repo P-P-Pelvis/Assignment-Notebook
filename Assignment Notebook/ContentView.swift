1b68fd8f4b6ad6c8c802e338e1cbe9fc0e64d94d
Edwin Tovar <poohwinnie2800@gmail.com>
2026-02-03 16:59:51 -0800

    Added Functionalty to the todo list move items, delete, and descriptions

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -8,17 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var assignmentItems =
+    [AssignmentItem(course: "Algebra", description: "Linears Equations", dueDate: Date()),
+     AssignmentItem(course: "History", description: "Civill War Paper", dueDate: Date()),
+     AssignmentItem(course: "Science", description: "Atomic Bomb Lap", dueDate: Date())
+    ]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(assignmentItems) { item in
+                    HStack {
+                        VStack(alignment: .leading, content: {
+                            Text(item.course).font(.headline)
+                            Text(item.description)
+                        })
+                        Spacer()
+                        Text(item.dueDate, style: .date)
+                    }
+                    
+                }
+                .onMove(perform: { indices, newOffset in
+                    assignmentItems.move(fromOffsets: indices, toOffset: newOffset)
+                })
+                .onDelete(perform: { indexSet in
+                    assignmentItems.remove(atOffsets: indexSet)
+                })
+            }
+            .navigationBarTitle("Assignment Notebook")
+            .navigationBarItems(leading: EditButton())
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+struct AssignmentItem : Identifiable {
+    var id = UUID()
+    var course = String()
+    var description = String()
+    var dueDate = Date()
 }
